6b44491b746ea90a9eebb2345413b94843ecc3a7
Prem Kumar.P <prem.kumar.ponuthorai@exact.com>
2018-05-04 19:06:27 +0200

    initial commit

---
+++ Person/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonNs
+{
+    public class Person
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(string firstname, string lastname)
+        {
+            _firstName = firstname;
+            _lastName = lastname;
+        }
+
+
+        public string GetFullName(string firstName, string lastName)
+        {
+            return string.Concat(_firstName, _lastName);
+        }
+    }
+}
---
+++ PersonTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonNs;
+
+namespace PersonTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person customer = new Person();
+            customer.FirstName = "Prem";
+            customer.LastName = "Kumar";
+
+            string FullName = customer.GetFullName("Prem ", "Kumar");
+
+            // Console.WriteLine(customer.FirstName);
+            Console.WriteLine(FullName);
+            Console.ReadLine();
+
+        }
+    }
+}
